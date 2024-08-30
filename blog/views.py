22cfcc2748069eb81ed2234af95cc544677d13bd
Henrique Pesciotti <hpesciotti@gmail.com>
2024-08-30 16:43:33 +0000

    Udpate PostList filter criteria

--- blog/views.py
+++ blog/views.py
@@ -11,5 +11,5 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    # template_name = "post_list.html"
