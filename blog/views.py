0ea2871acec37f155f214ea6cf536cbbe7680f04
Henrique Pesciotti <hpesciotti@gmail.com>
2024-09-05 16:57:12 +0000

    Fix postview for deployment

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-# from django.http import HttpResponse
 from django.views import generic
 from .models import Post
 
 # Create your views here.
 
-# Deleted
-# def index(request):
-#     return HttpResponse("Hello, Blog")
-
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    # Deleted
-    # template_name = "post_list.html" 
     template_name = "blog/index.html"
     paginate_by = 6
 
@@ -31,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
