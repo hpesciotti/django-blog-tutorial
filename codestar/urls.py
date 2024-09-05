0fc932057196aa41c7e2136179d4688104bf9057
Henrique Pesciotti <hpesciotti@gmail.com>
2024-09-05 19:03:18 +0000

    Fix postview for deployment

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     # Deleted
     # path('blog/', blog_views.index, name='index'),
-    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
