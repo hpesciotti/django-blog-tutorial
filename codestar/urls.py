dda2c679836ec1a3b0a3face5026185c52bebb04
Henrique Pesciotti <hpesciotti@gmail.com>
2024-09-07 17:47:58 +0000

    Add aunthetication package to codestar

--- codestar/urls.py
+++ codestar/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     # Deleted
     # path('blog/', blog_views.index, name='index'),
     path('about/', include('about.urls'), name='about-urls'),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('', include('blog.urls'), name='blog-urls'),
-    path('summernote/', include('django_summernote.urls')),
+    path('summernote/', include('django_summernote.urls')),    
 ]
